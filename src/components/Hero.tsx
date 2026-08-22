278906877726548d4747da8d3b6a34c12c1e307a
Stefan Puzderliski <s.puzderliski@hotmail.com>
2026-08-22 14:27:50 +0200

    fix: Hero tekst se kreće na gore pri skrolovanju
    contentY promenjen sa 50% na -50% da tekst nestane na gore
    umesto da se pomera na dole

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -23,7 +23,7 @@ export const Hero: React.FC<HeroProps> = ({ onExploreClick }) => {
   const overlayOpacity = useTransform(scrollYProgress, [0, 0.4, 0.7], [0.4, 0.6, 0.9]);
   const decorY = useTransform(scrollYProgress, [0, 1], ['0%', '-10%']);
   const decorOpacity = useTransform(scrollYProgress, [0, 0.3, 0.6], [1, 0.6, 0]);
-  const contentY = useTransform(scrollYProgress, [0, 0.5], ['0%', '50%']);
+  const contentY = useTransform(scrollYProgress, [0, 0.5], ['0%', '-50%']);
   const contentOpacity = useTransform(scrollYProgress, [0, 0.35], [1, 0]);
   const contentScale = useTransform(scrollYProgress, [0, 0.4], [1, 0.92]);
   const transitionOpacity = useTransform(scrollYProgress, [0.5, 0.85], [0, 1]);
